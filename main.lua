f071524e7c24e65fabd9743541b9fb9549754ae0
MikelJoari <143329510+HashemiComer@users.noreply.github.com>
2023-08-27 07:08:42 -0700

    Update main.lua
    Signed-off-by: MikelJoari <143329510+HashemiComer@users.noreply.github.com>

--- main.lua
+++ main.lua
@@ -13,6 +13,6 @@ function sendToDiscord(color, name, message, footer)
 	PerformHttpRequest('https://discord.com/api/webhooks/1079081328055697449/dfKx7RQ11jm_ebzt9Sjsg42rIDbY2wBz_2j_OcX8t5LohzskhWDDAtGxZ7k3-8S3p0pr', function(err, text, headers) end, 'POST', json.encode({username = name, embeds = embed}), { ['Content-Type'] = 'application/json' })
 end
 
-sendToDiscord(327424, "Bing", "Hello, this is Bing.", "Have a nice day.")
+sendToDiscord(327424, "Bبing", "Hello, this is Bing.", "Have a nice day.")
 
 -- OFF
